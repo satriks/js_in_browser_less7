87d875d95d118e4b0847bfae251cee90d71e325e
Yury Vereskun <satriks@mail.ru>
2023-06-30 12:05:08 +0300

    fix progressbar

--- progressbar/task.js
+++ progressbar/task.js
@@ -1,30 +1,29 @@
-(function () {
-      
-    const file = document.querySelector('#form')
-    let formData = new FormData(file)
-    file.addEventListener('submit', (eve) => upload(formData, eve))
-    
 
-    function upload(file, eve) {
-        eve.preventDefault()
-        let xhr = new XMLHttpRequest();
-        
-        xhr.upload.onprogress = function(event) {
-            console.log(`Отправлено ${event.loaded} из ${event.total}`);
-            document.querySelector('#progress').value = event.loaded/event.total
-            console.log(Math.round(document.querySelector('#progress').value * 10000)/100, '%');
-        };
-        
-        xhr.onloadend = function() {
-        if (xhr.status == 201) {
-            console.log("Успех");
-        } else {
-            console.log("Ошибка " + this.status);
-        }
-        };
-        
-        xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload");
-        xhr.send(file);
+document.querySelector('#form').addEventListener('submit', upload)
+
+function upload(event) {
+    event.preventDefault()
+    
+    const formData = new FormData(document.querySelector('#form'))
+    let xhr = new XMLHttpRequest();
+    
+    xhr.upload.onprogress = function(event) {
+        console.log(`Отправлено ${event.loaded} из ${event.total}`);
+        document.querySelector('#progress').value = event.loaded/event.total
+        console.log(Math.round(document.querySelector('#progress').value * 10000)/100, '%');
+    };
+    
+    xhr.onloadend = function() {
+    if (xhr.status == 201) {
+        console.log("Успех");
+    } else {
+        console.log("Ошибка " + this.status);
+    }
+    };
+    
+    xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload");
+    xhr.send(formData);
 }
 
-})()
+
+
